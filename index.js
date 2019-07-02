29ddc621adef9638de175c0aae02f8176d25abd4
Robin Timmis <robintimmis@gmail.com>
2019-07-02 18:06:05 +0100

    Added a simple multiplier

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+const readline = require('readline-sync');
+
 console.log('Welcome to the calculator!');
+
+console.log('Multiplication. Please enter a number:');
+const number1 = readline.prompt();
+
+console.log('Please enter another number:');
+const number2 = readline.prompt();
+
+
+console.log((+number1)*(+number2))
+
