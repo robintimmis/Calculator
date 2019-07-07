42b3a4866e16cf62a40cae4ad4995a2e9a473781
Robin Timmis <robintimmis@gmail.com>
2019-07-07 13:07:27 +0100

    Including comments (questions) after seeing sample solutions

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const operator =  readline.prompt();
 console.log('\nHow many numbers do you want to ' + operator + '?')
 const iterations = +readline.prompt();
 
-let number_arr = [];
+let number_arr = [];              //in sample solution starts with correct sized array, which is better?
 for (let i = 1; i <= iterations;i++) { 
     console.log('\nPlease enter number ' + i + ':');
     const argument = readline.prompt();
@@ -20,7 +20,7 @@ for (let i = 1; i <= iterations;i++) {
 let answer = number_arr[0];
 switch (operator) {
     case '+':
-        for (var i of number_arr.slice(1,)) {
+        for (var i of number_arr.slice(1,)) {     //in sample solution use for loop not for .. of, which better?
            answer += i;
         }
         break;
